75e970a99f21e06c50555f5a6f312c45c832be1e
tayeblagha <59334659+tayeblagha@users.noreply.github.com>
2023-02-22 01:02:14 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,15 +1,2 @@
-def calculate_sum(a, b):
-    """
-    Calculate the sum of two numbers
-
-    Args:
-        a (int): The first number
-        b (int): The second number
-
-    Returns:
-        int: The sum of the two numbers
-    """
-    return a + b
-
-print(calculate_sum(2, 3))
+print("hello word")
 
